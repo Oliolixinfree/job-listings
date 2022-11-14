ff1cbde279932da8f01c0a3681c3417db608dcc9
Ivan <offlane@tut.by>
2022-11-14 21:49:39 +0300

    Механизм добавления фильтров

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { addFilter } from 'store/filters/filter-actions';
 import { selectAllPositions } from 'store/positions/position-selectors';
 import { JobPosition } from './JobPosition';
 
 const JobList = () => {
+  const dispatch = useDispatch();
   const positions = useSelector(selectAllPositions);
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
 
   return (
     <div className="job-list">
       {positions.map((item) => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition key={item.id} handleAddFilter={handleAddFilter} {...item} />
       ))}
     </div>
   );
